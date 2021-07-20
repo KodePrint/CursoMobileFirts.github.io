d9ad95ae706de7ffb17ea00d3080f429a840286c
Kevin Palma <kapalma05@gmil.com>
2021-07-20 10:56:51 -0600

    Se termino de maquetar y dar estilos a la landinpage tambien se maqueto para tablets y equipos de escritorio

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+const tablesRight = document.getElementById('tablasRight');
+const tablesLeft = document.getElementById('tablesLeft');
+const tablasSlider = document.querySelector('#tableMonedas')
+
+console.log(tablasSlider)
+tablesRight.addEventListener('click', () => {
+    console.log('Quieres ir a comisiones')
+});
